0f418ba4d6ca19a4549862e38bbc9b41b27366f6
Nam Yen <vonamyen@gmail.com>
2025-09-23 08:56:09 +0700

    done

--- login.php
+++ login.php
@@ -13,37 +13,40 @@ if (!empty($_POST['submit'])) {
     ];
     $user = NULL;
     if ($user = $userModel->auth($users['username'], $users['password'])) {
-        //Login successful
+        // Login successful
         $_SESSION['id'] = $user[0]['id'];
-
         $_SESSION['message'] = 'Login successful';
-        header('location: list_users.php');
-    }else {
-        //Login failed
-        $_SESSION['message'] = 'Login failed';
+?>
+        <script>
+            localStorage.setItem("user_id", "<?php echo $_SESSION['id']; ?>");
+            window.location.href = "list_users.php";
+        </script>
+<?php
+        exit;
     }
-
 }
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
+
 <body>
-<?php include 'views/header.php'?>
+    <?php include 'views/header.php' ?>
 
     <div class="container">
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info" >
+            <div class="panel panel-info">
                 <div class="panel-heading">
                     <div class="panel-title">Login</div>
                     <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
                 </div>
 
-                <div style="padding-top:30px" class="panel-body" >
+                <div style="padding-top:30px" class="panel-body">
                     <form method="post" class="form-horizontal" role="form">
 
                         <div class="margin-bottom-25 input-group">
@@ -71,10 +74,10 @@ if (!empty($_POST['submit'])) {
 
                         <div class="form-group">
                             <div class="col-md-12 control">
-                                    Don't have an account!
-                                    <a href="form_user.php">
-                                        Sign Up Here
-                                    </a>
+                                Don't have an account!
+                                <a href="form_user.php">
+                                    Sign Up Here
+                                </a>
                             </div>
                         </div>
                     </form>
@@ -84,4 +87,13 @@ if (!empty($_POST['submit'])) {
     </div>
 
 </body>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        // Nếu login thành công (PHP đã set session)
+        <?php if (!empty($_SESSION['id'])): ?>
+            localStorage.setItem("user_id", "<?php echo $_SESSION['id']; ?>");
+        <?php endif; ?>
+    });
+</script>
+
 </html>
