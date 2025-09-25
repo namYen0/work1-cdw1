6cf66c6aeed2413839273be905ad83b7a1435d75
Nam Yen <vonamyen@gmail.com>
2025-09-25 07:37:17 +0700

    phong thu csrf

--- delete_user.php
+++ delete_user.php
@@ -1,13 +1,20 @@
 <?php
+session_start();
 require_once 'models/UserModel.php';
-$userModel = new UserModel();
 
-$user = NULL; //Add new user
+// Verify CSRF token
+if (empty($_GET['csrf_token']) || $_GET['csrf_token'] !== $_SESSION['csrf_token']) {
+    $_SESSION['message'] = 'CSRF token invalid!';
+    header('location: list_users.php');
+    exit();
+}
+
+$userModel = new UserModel();
 $id = NULL;
 
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
-    $userModel->deleteUserById($id);//Delete existing user
+    $userModel->deleteUserById($id);
 }
+
 header('location: list_users.php');
-?>
